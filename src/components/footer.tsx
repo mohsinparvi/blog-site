56608b23ca479b9e806a43b37af82fdb6e1a7f40
Mohsin Hassan <86055307+mohsinparvi@users.noreply.github.com>
2024-03-26 19:13:36 +0500

    design: footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,5 +1,25 @@
+import logo from "../assets/logo.svg";
+import FooterItem from "./footer/footer-item";
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="bg-tertiary">
+      <div className="max-w-[1140px] m-auto w-full">
+        <div className="flex  justify-between py-20">
+          <div className="flex flex-col ">
+            <img src={logo} alt="logo" />
+            <p>Build a modern and creative website with moonfo</p>
+            <div className="flex gap-2">
+              <div className="bg-[#FC5A5A] rounded-full h-8 w-8"></div>
+              <div className="bg-[#FC5A5A] rounded-full h-8 w-8"></div>
+              <div className="bg-[#FC5A5A] rounded-full h-8 w-8"></div>
+              <div className="bg-[#FC5A5A] rounded-full h-8 w-8"></div>
+            </div>
+          </div>
+          <FooterItem />
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
---
+++ src/components/footer/footer-item.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const FooterItem = () => {
+  return <div>FooterItem</div>;
+};
+
+export default FooterItem;
